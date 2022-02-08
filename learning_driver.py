a3a59b96ae9a8a011d7abda9acc69e06b2d41154
DjinnjaBeard <bowerashton1@gmail.com>
2022-02-08 10:44:12 -0700

    updated logging in learning_driver

--- learning_driver.py
+++ learning_driver.py
@@ -1,4 +1,6 @@
 from WordleAI.Wordle_Solver import output_best_weights
+from WordleAI import logging
 
 if __name__ == '__main__':
+	logging.getLogger('learn_thresholds_and_weights').setLevel(logging.DEBUG)
 	output_best_weights()
